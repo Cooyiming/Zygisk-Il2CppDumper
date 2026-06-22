0eced119b4d1351e0881bec71c75c48a2fc4358c
Cooyiming <52604402+Cooyiming@users.noreply.github.com>
2026-06-22 15:09:57 +0800

    更新global-metadata.dat支持

--- module/src/main/cpp/hack.h
+++ module/src/main/cpp/hack.h
@@ -9,4 +9,12 @@
 
 void hack_prepare(const char *game_data_dir, void *data, size_t length);
 
+// The address (relative to libil2cpp base) of the pointer that points to the
+// decrypted global metadata block.
+#define GlobalMetadataAddr 0x3DE7930UL
+
+// Metadata header offsets used to locate the blob size information.
+#define GlobalMetadataDefinitionsOffset 0x100
+#define GlobalMetadataDefinitionsCountOffset 0x104
+
 #endif //ZYGISK_IL2CPPDUMPER_HACK_H
